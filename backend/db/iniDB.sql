34818001f1b8156f028573e21bdb395f47264748
miguelvidalbellido <miguel.vidal.bell@gmail.com>
2023-11-13 15:40:52 +0100

    Deploy docker

---
+++ backend/db/iniDB.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE test2;
+
+CREATE TABLE test2.users (
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(45) NOT NULL,
+    password VARCHAR(45) NOT NULL,
+    PRIMARY KEY (id)
+);
